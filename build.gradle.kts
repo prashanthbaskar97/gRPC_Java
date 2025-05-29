ea58a8208155b8e70e4d78d5e818e39fe368ae5f
prashanthbaskar97 <baskar.pr@northeastern.edu>
2025-05-29 18:55:24 -0400

    Initial commit: Java gRPC Gradle project setup

---
+++ build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    id("com.google.protobuf") version "0.9.5"
+    id("java")
+
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    // https://mvnrepository.com/artifact/com.google.protobuf/protobuf-java
+    implementation("com.google.protobuf:protobuf-java:3.21.12")
+    // https://mvnrepository.com/artifact/io.grpc/grpc-all
+    implementation("io.grpc:grpc-all:1.72.0")
+}
+
+
+sourceSets{
+    main{
+        java{
+            srcDirs("build/generated/source/proto/main/grpc")
+            srcDirs("build/generated/source/proto/main/java")
+
+        }
+    }
+}
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.21.12"
+    }
+
+    plugins {
+        create("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.72.0"
+        }
+    }
+
+    generateProtoTasks {
+        all().forEach { task ->
+            task.plugins {
+                create("grpc")
+            }
+        }
+    }
+
+}
+
+
+
